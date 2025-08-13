056b4447fa19330b3fa03ddce8f44646d6880c75
Tim <73362594+TimCheers@users.noreply.github.com>
2025-08-13 20:18:12 +0900

    routes (not done)

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React, { createContext } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import UserStore from "./store/UserStore";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Context = createContext(null);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Context.Provider
+    value={{
+      user: new UserStore(),
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Context.Provider>
 );
